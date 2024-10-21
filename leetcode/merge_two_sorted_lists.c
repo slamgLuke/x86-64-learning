c2e4a74b125cb0947790120090e123016f163d2d
Lucas Carranza <lucas.carranza@utec.edu.pe>
2024-10-21 13:21:19 -0500

    added leetcode problems

---
+++ leetcode/merge_two_sorted_lists.c
@@ -0,0 +1,121 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    /*
+        head: rdi = 0
+        ptr: rsi = ?
+        it1: rdx, it2: rcx
+        while (it1 != 0 && it2 != 0) {
+            if (*(it1) <= *(it2))
+                rax = it1
+                it1 = *(it1 + 4)
+            else
+                rax = it2
+                it2 = *(it2 + 4)
+            
+            if (head == 0)
+                    head = rax
+                    ptr = rax
+                else
+                    *(ptr + 4) = rax
+                    ptr = rax
+        }
+
+        while (it1 != 0) {
+            if (head == 0)
+                    head = it1
+                    ptr = it1
+                else
+                    *(ptr + 4) = it1
+                    ptr = it1
+            it1 = *(it1 + 4)
+        }
+
+        while (it2 != 0) {
+            if (head == 0)
+                    head = it2
+                    ptr = it2
+                else
+                    *(ptr + 4) = it2
+                    ptr = it2
+            it2 = *(it2 + 4)
+        }
+    */
+
+    /*
+        stack diagram:
+        rbp - 8  = list1
+        rbp - 16 = list2
+    */
+
+    __asm__ (
+    "movq %%rdi, %%rdx;\n"
+    "movq %%rsi, %%rcx;\n"
+    "xorq %%rdi, %%rdi;\n"
+    "xorq %%rsi, %%rsi;\n"
+    "loop0%=:\n"
+    "cmpq $0, %%rdx;\n"
+    "je endloop0%=;\n"
+    "cmpq $0, %%rcx;\n"
+    "je endloop0%=;\n"
+    "movl (%%rdx), %%eax;\n"
+    "cmpl (%%rcx), %%eax;\n"
+    "jg else1%=;\n"
+    "movq %%rdx, %%rax;\n"
+    "movq 8(%%rdx), %%rdx;\n"
+    "jmp endif1%=;\n"
+    "else1%=:\n"
+    "movq %%rcx, %%rax;\n"
+    "movq 8(%%rcx), %%rcx;\n"
+    "endif1%=:\n"
+    "cmpq $0, %%rdi;\n"
+    "jne else2%=;\n"
+    "movq %%rax, %%rdi;\n"
+    "movq %%rax, %%rsi;\n"
+    "jmp endif2%=;\n"
+    "else2%=:\n"
+    "movq %%rax, 8(%%rsi);\n"
+    "movq %%rax, %%rsi;\n"
+    "endif2%=:\n"
+    "jmp loop0%=;\n"
+    "endloop0%=:\n"
+    "loop1%=:\n"
+    "cmpq $0, %%rdx;\n"
+    "je endloop1%=;\n"
+    "cmpq $0, %%rdi;\n"
+    "jne else3%=;\n"
+    "movq %%rdx, %%rdi;\n"
+    "movq %%rdx, %%rsi;\n"
+    "jmp endif3%=;\n"
+    "else3%=:\n"
+    "movq %%rdx, 8(%%rsi);\n"
+    "movq %%rdx, %%rsi;\n"
+    "endif3%=:\n"
+    "movq 8(%%rdx), %%rdx;\n"
+    "jmp loop1%=;\n"
+    "endloop1%=:\n"
+    "loop2%=:\n"
+    "cmpq $0, %%rcx;\n"
+    "je endloop2%=;\n"
+    "cmpq $0, %%rdi;\n"
+    "jne else4%=;\n"
+    "movq %%rcx, %%rdi;\n"
+    "movq %%rcx, %%rsi;\n"
+    "jmp endif4%=;\n"
+    "else4%=:\n"
+    "movq %%rcx, 8(%%rsi);\n"
+    "movq %%rcx, %%rsi;\n"
+    "endif4%=:\n"
+    "movq 8(%%rcx), %%rcx;\n"
+    "jmp loop2%=;\n"
+    "endloop2%=:\n"
+    "movq %%rdi, %%rax;\n"
+    "ret" : : );
+
+    return NULL;
+}
---
+++ leetcode/two_sum.c
@@ -0,0 +1,54 @@
+// This one was actually made following Low Level Learning's video
+// https://www.youtube.com/watch?v=lALPErFlfNQ
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    __asm__(".intel_syntax noprefix;\n\
+    push rbx;\n\
+    sub rsp, 0x40;\n\
+    mov qword ptr [rsp+0x10], rdi;\n\
+    mov dword ptr [rsp+0x18], esi;\n\
+    mov dword ptr [rsp+0x20], edx;\n\
+    mov qword ptr [rsp+0x28], rcx;\n\
+    mov rdi, 8;\n\
+    call malloc;\n\
+    mov qword ptr [rsp+0x30], rax;\n\
+    xor r8, r8;\n\
+    for1%=:\n\
+    mov r9, r8;\n\
+    add r9, 1;\n\
+    for2%=:\n\
+    mov rbx, qword ptr [rsp+0x10];\n\
+    xor rax, rax;\n\
+    lea rbx, [rbx+r8*4];\n\
+    add eax, dword ptr [rbx];\n\
+    mov rbx, qword ptr [rsp+0x10];\n\
+    lea rbx, [rbx+r9*4];\n\
+    add eax, dword ptr [rbx];\n\
+    mov ebx, dword ptr [rsp+0x20];\n\
+    cmp ebx, eax;\n\
+    jne continue%=;\n\
+    mov rax, qword ptr [rsp+0x30];\n\
+    mov dword ptr [rax], r8d;\n\
+    lea rax, [rax+4];\n\
+    mov dword ptr [rax], r9d;\n\
+    mov rax, qword ptr [rsp+0x28];\n\
+    mov dword ptr [rax], 0x2;\n\
+    jmp leave%=;\n\
+    continue%=:\n\
+    inc r9;\n\
+    cmp r9d, dword ptr [rsp+0x18];\n\
+    jle for2%=;\n\
+    inc r8;\n\
+    cmp r8d, dword ptr [rsp+0x18];\n\
+    jle for1%=;\n\
+    leave%=:\n\
+    mov rax, qword ptr [rsp+0x30];\n\
+    add rsp, 0x40;\n\
+    pop rbx;\n\
+    ret;\n\
+    .att_syntax;" : :);
+    return NULL;
+}
